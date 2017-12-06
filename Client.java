814c54cb8c0b187c0cfa75d9992a1de247a57508
073274474 <33285975+073274474@users.noreply.github.com>
2017-12-05 20:47:28 -0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,7 +1,26 @@
 class Client{
   String name = "";
+  String clientIP = "";
   
-  Client(){
+  Client(String name, String clientIP){
+    this.name = name;
+    this.clientIP = clientIP;
+  }
+  
+  public String getName(){
+    return this.name;
+  }
+  
+  public String getIP(){
+    return this.name;
+  }
+  
+  public void setName(String name){
     this.name = name;
   }
+  
+  public void setIP(String clientIP){
+    this.clientIP = clientIP;
+  }
+    
 }
